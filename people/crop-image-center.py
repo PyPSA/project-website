9058cf57f51a474826e158cbac7e050864deee3c
Fabian <fab.hof@gmx.de>
2021-10-23 19:09:53 +0200

    add MV profile picture, fix name

--- people/crop-image-center.py
+++ people/crop-image-center.py
@@ -23,6 +23,6 @@ def centercrop(path):
     return im.crop((left, top, right, bottom))
 
 
-for path in ['fabianhofmann.jpg', 'fabianneumann.jpg', 'tombrown.jpg']:
+for path in ['martavictoria.jpg']:
     name = path.split('.')[0]
     centercrop(path).save(name+'_cropped.jpg')
